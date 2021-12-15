7d8fcb4312d23ca8844d98566200e602a2ffccbf
PsychedelicMonkey <ryan.getz.96@gmail.com>
2021-12-15 10:14:14 -0700

    Product page title

--- routes/products.js
+++ routes/products.js
@@ -7,7 +7,7 @@ router.get('/:id', async (req, res, next) => {
 	try {
 		const product = await Product.findById(req.params.id);
 
-		return res.render('product', { product });
+		return res.render('product', { title: product.name, product });
 	} catch (err) {
 		return next(err);
 	}
